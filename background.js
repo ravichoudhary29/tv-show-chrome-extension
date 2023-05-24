12a4ef9b6c0e4aba86350d0a3368af7e914e0af5
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-05-24 17:42:32 +0530

    Complete Chrome Runtime and Context Menu APIs.

---
+++ background.js
@@ -0,0 +1,27 @@
+chrome.runtime.onInstalled.addListener((details) => {
+    chrome.contextMenus.create({
+        title: "Test Context Menu 1",
+        id: "contextMenu1",
+        contexts: ["page", "selection"],
+    });
+
+    chrome.contextMenus.onClicked.addListener((event) => {
+        console.log(event);
+    });
+
+    chrome.contextMenus.create({
+        title: "Test Context Menu 2",
+        id: "contextMenu2",
+        parentId: "contextMenu1",
+        contexts: ["page", "selection"],
+    });
+
+    chrome.contextMenus.create({
+        title: "Test Context Menu 3",
+        id: "contextMenu3",
+        parentId: "contextMenu1",
+        contexts: ["page", "selection"],
+    });
+});
+
+console.log("background script running.");
