ceb7b869803018bea08f2461fe49d03ae4f00592
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-05-25 12:28:12 +0530

    Complete Data fetching/HTTP Requests.

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 chrome.runtime.onInstalled.addListener((details) => {
+    chrome.storage.local.set({
+        shows: [],
+    });
     chrome.contextMenus.create({
         title: "Test Context Menu 1",
         id: "contextMenu1",
@@ -6,16 +9,13 @@ chrome.runtime.onInstalled.addListener((details) => {
     });
 
     chrome.contextMenus.onClicked.addListener((event) => {
-        console.log(event);
-        chrome.tabs.create({
-            url: "https://www.imdb.com/chart/toptv/",
-        });
+        fetch(`http://api.tvmaze.com/search/shows?q=${event.selectionText}`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                chrome.storage.local.set({
+                    shows: data,
+                });
+            });
     });
-});
-
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    console.log(msg);
-    console.log(sender);
-    sendResponse("recieved message from background.");
-    chrome.tabs.sendMessage(sender.tab.id, "Got your message from background");
 });
